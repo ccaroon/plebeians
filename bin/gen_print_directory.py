db2c2dd8455d5ef8d7eaa275b81a9cfbf03ef14a
Craig N. Caroon <ccaroon@webassign.net>
2018-10-05 10:03:25 -0400

    Added ability to render families on LEFT and RIGHT side of page.

--- bin/gen_print_directory.py
+++ bin/gen_print_directory.py
@@ -3,6 +3,7 @@
 # ------------------------------------------------------------------------------
 ################################################################################
 from datetime import datetime
+import itertools
 import json
 import os.path as path
 import sys
@@ -14,12 +15,15 @@ from reportlab.lib.units import cm, mm, inch, pica
 class Directory:
     FONT = "Times-Roman"
 
+    SIDE_LEFT  = 0
+    SIDE_RIGHT = 1
+
     def __init__(self, data_path):
         self.__data_path = data_path
 
         # Create and Prep page
         output_name = path.basename(data_path)
-        self.__pdf = Canvas("%s.pdf" % (output_name), pagesize=landscape(letter))
+        self.__pdf = Canvas("%s.pdf" % (output_name), pagesize=landscape(letter), pageCompression=0)
 
         # Read Directory Data
         self.__member_data = None
@@ -28,38 +32,60 @@ class Directory:
             self.__member_data = json.load(fh)
 
     def render(self):
-        # Render a TEST family
-        self.render_family(self.__member_data[54])
+        side_it = itertools.cycle((Directory.SIDE_LEFT, Directory.SIDE_RIGHT))
+
+        num_members = len(self.__member_data)
+        num_members = 10
+        for i in xrange(0, num_members, 2):
+        # for family in self.__member_data:
+            # Vertical Line Down Center
+            self.__pdf.setStrokeColorRGB(0,0,0)
+            self.__pdf.line(5.5*inch,0*inch, 5.5*inch, 8.5*inch)
+
+            # Render the Family
+            self.render_family(self.__member_data[i], side_it.next())
+            if i+1 < num_members:
+                self.render_family(self.__member_data[i+1], side_it.next())
+
+            # End the Page
+            self.__pdf.showPage()
 
         # Save PDF
         self.__pdf.save()
 
-    def render_family(self, family):
-        # Vertical Line Down Center
-        self.__pdf.setStrokeColorRGB(0,0,0)
-        self.__pdf.line(5.5*inch,0*inch, 5.5*inch, 8.5*inch)
+    def render_family(self, family, side):
+        left_margin  = 0
+        right_margin = 0
+        if side == Directory.SIDE_LEFT:
+            left_margin  = .25*inch
+            right_margin = 5.5*inch
+        elif side == Directory.SIDE_RIGHT:
+            left_margin  = 5.5*inch + .25*inch
+            right_margin = 11*inch
+        else:
+            raise ValueError("Invalid value [%s] for 'side' parameter." % (side))
 
         # Family Name - Left
         self.__pdf.setFont(Directory.FONT, 18)
-        draw_pos = {'x': .25*inch, 'y': 8.125*inch}
+        draw_pos = {'x': left_margin, 'y': 8.125*inch}
         self.__pdf.drawString(draw_pos['x'], draw_pos['y'], family['name'])
 
         # Family Address - Left
         self.__pdf.setFont(Directory.FONT, 16)
         draw_pos['y'] -= .20*inch
         addr = self.__pdf.beginText(draw_pos['x'], draw_pos['y'])
-        addr.setLeading(12)
+        addr.setLeading(15)
         addr.textLine(family['address1'])
         addr.textLine("%s, %s %s" % (family['city'], family['state'], family['zip']))
         self.__pdf.drawText(addr)
 
         # Family Name/Address Divider - Left
         self.__pdf.setStrokeColorRGB(.5, .5, .5)
-        self.__pdf.line(0.25*inch, 7.5*inch, 5.25*inch, 7.5*inch)
+        self.__pdf.line(left_margin, 7.5*inch, right_margin-(0.25*inch), 7.5*inch)
 
         # Member Photo, Info
         photo_dir = "%s/photos" % (self.__data_path)
-        pos_x = 0.25*inch
+        pos_x = left_margin
         pos_y = 5.25*inch
         for person in family['members']:
             print "%s - %s" % (person['name'], pos_x)
@@ -102,11 +128,9 @@ class Directory:
             self.__pdf.drawText(info)
 
             pos_x += 2.70*inch
-            if pos_x >= 5.5*inch:
-                pos_x = 0.25*inch
+            if pos_x >= right_margin:
+                pos_x = left_margin
                 pos_y = 1.90*inch
-
-        self.__pdf.showPage()
 
 # ------------------------------------------------------------------------------
 def main():
